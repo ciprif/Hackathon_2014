3796f299df2108abbeeef19fa7e6ab4aa878ae7e
Radu Pascal <haiduc32@gmail.com>
2014-11-16 01:29:39 +0200

    added authorization for FitBit

---
+++ Hackathon/ProjektB.Web/Models/FitnessProviderFBPayload.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektB.Web.Models
+{
+    public class FitnessProviderFBPayload
+    {
+        public string AuthToken { get; set; }
+
+        public string AuthTokenSecret { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
