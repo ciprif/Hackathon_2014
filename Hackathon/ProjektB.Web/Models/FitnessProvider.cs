92f525b34f3d9bca97ab6f53d606e71136f88b53
mishacucicea <mishacucicea@yahoo.com>
2014-11-15 16:08:34 +0200

    Adding FitnessProvider

---
+++ Hackathon/ProjektB.Web/Models/FitnessProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektB.Web.Models
+{
+    public class FitnessProvider
+    {
+        public int Id { get; set; }
+
+        public string ConnectionDetails { get; set; }
+
+        public ProviderType Type { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    public enum ProviderType
+    {
+        FitBit = 1,
+        MapMyFitness = 2,
+    }
+}
