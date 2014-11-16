01ae98ca9cddd1e5f4b6dc1f9b50b54ac50c5e63
mishacucicea <mishacucicea@yahoo.com>
2014-11-16 04:13:14 +0200

    Leader board adjustments

---
+++ Hackathon/ProjektB.Web/Models/ModelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektB.Web.Models
+{
+    public static class ModelExtensions
+    {
+        public static double GetScore(this IGrouping<string, ActivityViewModel> activityGroup)
+        {
+            return (activityGroup.Sum(a => a.Score) / activityGroup.GroupBy(g => g.UserName).Count());
+        }
+    }
+}
